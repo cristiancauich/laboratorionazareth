093a2d47d59cab8fb73ebe02b884c4a0be2cac58
Eduardo Serena <eduardoserenag@gmail.com>
2025-10-25 14:10:44 -0600

    UPDATE loader

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup>
+	import { onMounted } from "vue";
 	import Header from "./components/Header.vue";
 	import Footer from "./components/Footer.vue";
+
+	onMounted(() => {
+        window.addEventListener('load', () => {
+			document.body.classList.add('loaded')
+			const loader = document.getElementById('loader')
+			if (loader) loader.remove()
+		})
+    })
 </script>
 
 <template>
